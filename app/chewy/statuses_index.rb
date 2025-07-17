06f9100e9cd51902a025187c28912dd982af1251
zhxchen17 <zhxchen17@outlook.com>
2025-07-16 23:49:32 -0400

    patches

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -21,9 +21,18 @@ class StatusesIndex < Chewy::Index
       },
     },
 
+    char_filter: {
+      tsconvert: {
+        type: 'stconvert',
+        keep_both: false,
+        delimiter: '#',
+        convert_type: 't2s',
+      },
+    },
+
     analyzer: {
       verbatim: {
-        tokenizer: 'uax_url_email',
+        tokenizer: 'ik_max_word',
         filter: %w(lowercase),
       },
 
@@ -38,6 +47,7 @@ class StatusesIndex < Chewy::Index
           english_stop
           english_stemmer
         ),
+        char_filter: %w(tsconvert),
       },
 
       hashtag: {
